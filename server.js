e599481493141ed3a1d0992ed34c83fe57f7ccdc
tdjohnson7 <tdjohnson7@msn.com>
2023-08-04 16:56:15 -0500

    commit

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ MongoClient.connect(dbConnectionString, {useUnifiedTopology: true, useNewUrlPars
         collection = db.collection('AOE42ndCollection')
         collection2 = db.collection('AOE4TechCollection')
     })
+    .then(app.listen(process.env.PORT || PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    }))
     .catch(err=> console.log("ERROR connecting to DB: ", err))
 
 app.set('view engine', 'ejs')
@@ -840,7 +843,5 @@ app.post('/calculate', async (request, response)=>{
 
 
 //PORT = 8000
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+
 
